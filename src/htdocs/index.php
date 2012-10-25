e394a7cbfd544964d6a2042bd64aa670bec03dd3
Kore Nordmann <mail@kore-nordmann.de>
2012-10-25 16:14:00 +0200

    Added: Global try-catch statement for exceptions
    Outputs exceptions. This makes sense for an internal tool and shows
    errors for those with a PHP disregading any errors by configuration.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -9,32 +9,42 @@
 namespace Qafoo\Review;
 use Qafoo\RMF;
 
-require __DIR__ . '/../main/Qafoo/Review/bootstrap.php';
-$dic = new DIC\Base();
+try
+{
+    require __DIR__ . '/../main/Qafoo/Review/bootstrap.php';
+    $dic = new DIC\Base();
 
-$dic->mysqli;
+    $dic->mysqli;
 
-$dispatcher = new RMF\Dispatcher\Simple(
-    new RMF\Router\Regexp( array(
-        '(^/$)' => array(
-            'GET'  => array( $dic->reviewController, 'showOverview' ),
-        ),
-        '(^/show/(?P<analyzer>[A-Za-z_]+))' => array(
-            'GET'  => array( $dic->reviewController, 'showAnalyzer' ),
-        ),
-        '(^/source/annotate$)' => array(
-            'POST'  => array( $dic->sourceController, 'annotate' ),
-        ),
-        '(^/source/?(?P<path>.*)$)' => array(
-            'GET'  => array( $dic->reviewController, 'showSource' ),
-        ),
-    ) ),
-    $dic->view
-);
+    $dispatcher = new RMF\Dispatcher\Simple(
+        new RMF\Router\Regexp( array(
+            '(^/$)' => array(
+                'GET'  => array( $dic->reviewController, 'showOverview' ),
+            ),
+            '(^/show/(?P<analyzer>[A-Za-z_]+))' => array(
+                'GET'  => array( $dic->reviewController, 'showAnalyzer' ),
+            ),
+            '(^/source/annotate$)' => array(
+                'POST'  => array( $dic->sourceController, 'annotate' ),
+            ),
+            '(^/source/?(?P<path>.*)$)' => array(
+                'GET'  => array( $dic->reviewController, 'showSource' ),
+            ),
+        ) ),
+        $dic->view
+    );
 
-$request = new RMF\Request\HTTP();
-$request->addHandler( 'body', new RMF\Request\PropertyHandler\PostBody() );
-$request->addHandler( 'session', new RMF\Request\PropertyHandler\Session() );
+    $request = new RMF\Request\HTTP();
+    $request->addHandler( 'body', new RMF\Request\PropertyHandler\PostBody() );
+    $request->addHandler( 'session', new RMF\Request\PropertyHandler\Session() );
 
-$dispatcher->dispatch( $request );
+    $dispatcher->dispatch( $request );
+}
+catch ( \Exception $e )
+{
+    header( 'Status: 500 Internal Server Error' );
+    echo "<h1>Internal Server Error</h1>\n";
+    echo "<pre>$e</pre>";
+}
+
 
