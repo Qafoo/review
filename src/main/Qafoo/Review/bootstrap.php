715dc331210e027990ed3beb2454ac4de22be793
Kore Nordmann <mail@kore-nordmann.de>
2012-10-25 16:12:50 +0200

    Convert all errors into exceptions

--- src/main/Qafoo/Review/bootstrap.php
+++ src/main/Qafoo/Review/bootstrap.php
@@ -24,5 +24,36 @@ spl_autoload_register(
     }
 );
 
+set_error_handler(
+    function ( $type, $message, $file, $line )
+    {
+        if ( error_reporting() === 0 )
+        {
+            // This error has been intentionally silenced
+            return;
+        }
+
+        $mapping = array(
+            1     => 'E_ERROR',
+            2     => 'E_WARNING',
+            4     => 'E_PARSE',
+            8     => 'E_NOTICE',
+            16    => 'E_CORE_ERROR',
+            32    => 'E_CORE_WARNING',
+            64    => 'E_COMPILE_ERROR',
+            128   => 'E_COMPILE_WARNING',
+            256   => 'E_USER_ERROR',
+            512   => 'E_USER_WARNING',
+            1024  => 'E_USER_NOTICE',
+            2048  => 'E_STRICT',
+            4096  => 'E_RECOVERABLE_ERROR',
+            8192  => 'E_DEPRECATED',
+            16384 => 'E_USER_DEPRECATED',
+        );
+
+        throw new \RuntimeException( "{$mapping[$type]}: $message in $file +$line" );
+    }
+);
+
 // @codingStandardsIgnoreEnd
 // @codeCoverageIgnoreEnd
