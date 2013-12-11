61faed959222fa4ec30c84fb64dcaba20531358c
Kore Nordmann <mail@kore-nordmann.de>
2013-12-11 15:33:59 +0100

    Added: Missing dependency

--- src/htdocs/scripts/app.js
+++ src/htdocs/scripts/app.js
@@ -2,7 +2,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.
-    module('qaReview', ['qaReview.filters', 'qaReview.services', 'qaReview.directives', 'qaReview.controllers', 'ui.bootstrap', 'morris']).
+    module('qaReview', ['ngRoute', 'qaReview.filters', 'qaReview.services', 'qaReview.directives', 'qaReview.controllers', 'ui.bootstrap', 'morris']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: 'templates/home.html',
