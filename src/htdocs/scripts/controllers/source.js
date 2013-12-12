82c602f52d3b3a0c29c146a7c9937da44c8b2ba2
Kore Nordmann <mail@kore-nordmann.de>
2013-12-12 16:43:18 +0100

    Integrated Google Prettify
    Finally a JS source highlighter which somehow works and supports line
    numbers…

--- src/htdocs/scripts/controllers/source.js
+++ src/htdocs/scripts/controllers/source.js
@@ -1,11 +1,28 @@
 var Controller = Controller || {};
 
-Controller.Source = function ($scope, $http) {
+Controller.Source = function ($scope, $http, $sce) {
     $http.get('/results/source_tree.js').success(function(data) {
         $scope.source = {
             tree: data,
-            path: [],
-            selected: null
+            path: [data[0].name],
+            code: null
         };
     });
+
+    $scope.select = function(path, content) {
+        $scope.source.path = path;
+
+        if (content) {
+            $http.get('/results/' + content).success(function(data) {
+                $scope.source.code = $sce.trustAsHtml(
+                    prettyPrintOne(
+                        data.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;'),
+                        undefined,
+                        true
+                    )
+                )
+            });
+        }
+    };
 };
+
